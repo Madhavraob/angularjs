80697a9d659cb99f6321999102da4baffffecd17
Madhavraob <madhavrao039@gmail.com>
2020-04-29 19:02:59 +0530

    exclude tinymce

--- app/view1/view1.js
+++ app/view1/view1.js
@@ -12,9 +12,9 @@ angular.module('myApp.view1', ['ngRoute', 'mentio'])
 
   .controller('View1Ctrl', ['$scope', 'mentioUtil', function ($scope, mentioUtil) {
 
-    $scope.getTagTextRaw = function (item) {
-      return '<i class="mention-tag-text" style="color:#a52a2a;">' + item.name + '</i>';
-    };
+    // $scope.getTagTextRaw = function (item) {
+    //   return '<i class="mention-tag-text" style="color:#a52a2a;">' + item.name + '</i>';
+    // };
     
     $scope.getTagTextRaw = function (item) {
       return item.name;
@@ -92,19 +92,5 @@ angular.module('myApp.view1', ['ngRoute', 'mentio'])
       "id": "Q4",
       "name": "@Q4"
     }];
-
-    $scope.tinyMceOptions = {
-      init_instance_callback: function (editor) {
-        $scope.iframeElement = editor.iframeElement;
-      },
-      resize: false,
-      width: '100%',
-      height: 150,
-      plugins: 'print textcolor',
-      toolbar: "bold italic underline strikethrough| undo redo",
-      toolbar_items_size: 'small',
-      menubar: false,
-      statusbar: false
-    };
 
   }]);
